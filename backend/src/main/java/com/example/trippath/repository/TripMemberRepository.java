29b7345a506c6e146649a62d42634e8d63a62d01
Natsuki Tokunaga <natsuki.tokunaga@WESERVE.CO.JP>
2025-02-06 17:35:57 +0900

    修正漏れ

--- backend/src/main/java/com/example/trippath/repository/TripMemberRepository.java
+++ backend/src/main/java/com/example/trippath/repository/TripMemberRepository.java
@@ -10,6 +10,6 @@ import com.example.trippath.entity.TripMember;
 
 public interface TripMemberRepository extends JpaRepository<TripMember, Integer> {
     
-    @Query("SELECT tm FROM TripMember tm WHERE tripId = :id")
+    @Query("SELECT tm FROM TripMember tm WHERE tripId = :id AND deletedAt Is NULL")
     public List<TripMember> findAllById(Integer id);
 }
