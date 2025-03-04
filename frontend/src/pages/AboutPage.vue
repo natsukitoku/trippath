f6ebbf8e8ba2885166e25c8abbce031b68068f7a
Natsuki Tokunaga <natsuki.tokunaga@WESERVE.CO.JP>
2025-03-04 13:42:52 +0900

    AboutPage一応余計なものは削除

--- frontend/src/pages/AboutPage.vue
+++ frontend/src/pages/AboutPage.vue
@@ -1,35 +1,11 @@
 <template>
   <div>
     <h4>Vue + Spring Boot</h4>
-    <button @click="fetchTrips">Fetch trips</button>
-    <div v-if="trips">
-      <label v-for="trip in trips" :key="trip.id"><p>{{ trip }}</p></label>
-    </div>
   </div>
-
-  </template>
-  
-  <script>
-  import axios from 'axios';
+</template>
+<script>
   export default {
-    name: 'AboutPage',
-    data() {
-      return {
-        trips: null
-      };
-    },
-    methods: {
-      fetchTrips() {
-        axios
-            .get("http://localhost:8080/trip/index")
-            .then((response) => {
-              this.trips = response.data;
-            })
-            .catch((error) => {
-              console.error("There was an error:", error);
-            });
-      },
-    },
+      name: 'AboutPage'
   };
-  </script>
+</script>
   
